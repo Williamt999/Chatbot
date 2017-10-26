5caa80d0dc94212e0488c7fd59646d139dd5dfa1
William Tordo <williamtordo@MacBook-Pro-de-William.local>
2017-10-26 22:47:43 +0200

    fix typo

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var chatservice = require('../server/chatService');
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
-  res.render('webhook');
   if (chatservice.authenticate){
     return res.status(200).send(req.query['hub.challenge']);
   } else {
